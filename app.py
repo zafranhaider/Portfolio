aed79374ca7249978b4345d53b5cdecf6ae9fddf
Zafran <zraaeae@gmail.com>
2024-04-11 20:25:10 +0500

    First Push

---
+++ app.py
@@ -0,0 +1,48 @@
+from flask import Flask, render_template, redirect,request
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_migrate import Migrate
+from model import db, Contact
+from flask_mail import Mail, Message
+app = Flask(__name__, template_folder='templates')
+app.config['SECRET_KEY'] = '12345'  # Add your secret key here
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = 'zraaeae@gmail.com'  # Add your Gmail email here
+app.config['MAIL_PASSWORD'] = 'ozwd aodx ifpg lvfm'  # Add your Gmail password here
+
+# Initialize Flask extensions
+mail = Mail(app)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+#Contacts
+@app.route('/submit', methods=['POST'])
+def submit():
+    name = request.form['name']
+    email = request.form['email']
+    message = request.form['message']
+    
+    contact = Contact(name=name, email=email, message=message)
+    db.session.add(contact)
+    db.session.commit()
+        # Add the new contact to the database
+
+        # Send email notification
+    send_email_notification(name, email, message)
+    
+    return render_template('index.html')
+def send_email_notification(name, email, message):
+    msg = Message('Hiring Team',
+                  sender=email,
+                  recipients=['zraaeae@gmail.com'])  # Add your email here
+    msg.body = f"Name: {name}\nEmail: {email}\nMessage: {message}"
+    mail.send(msg)
+
---
+++ model.py
@@ -0,0 +1,9 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Contact(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    email = db.Column(db.String(100))
+    message = db.Column(db.Text)
